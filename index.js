5741ea7fb448e56bd007b6482ca1252e1afe5d46
Asif Ahmed <asifahmedakash32@gmail.com>
2026-06-21 13:03:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Home route
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
